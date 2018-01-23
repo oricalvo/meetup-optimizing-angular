43c6d638a91190713224a21fce616cdedcc71c02
oricalvo <ori.calvo@gmail.com>
2018-01-23 08:51:16 +0200

    More

---
+++ 02 hmr/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import {appStore} from './app.store';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  constructor() {
+    console.log("ctor");
+  }
+
+  get counter() {
+    return appStore.counter;
+  }
+
+  inc() {
+    ++appStore.counter;
+  }
+
+  dec() {
+    --appStore.counter;
+  }
+}
---
+++ 02 hmr/src/app/app.store.ts
@@ -0,0 +1,7 @@
+export interface AppStore {
+  counter: number;
+}
+
+export const appStore: AppStore = {
+  counter: 0,
+};
