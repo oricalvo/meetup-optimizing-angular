111674a5de909f08ca6c088fa25e0c957b12aa79
ocalvo <ori.calvo@gmail.com>
2018-01-23 14:54:51 +0200

    More

--- 01 counters/src/app/app.component.ts
+++ 01 counters/src/app/app.component.ts
@@ -10,19 +10,17 @@ declare var L;
 export class AppComponent {
   title = 'app';
 
-  constructor(private ngZone: NgZone) {
+  constructor() {
   }
 
   ngOnInit() {
-    this.ngZone.runOutsideAngular(()=> {
-      var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox.streets',
-        accessToken: 'pk.eyJ1Ijoib3JpY2Fsdm8iLCJhIjoiY2pjcW1uYjBhMTlzeDJxb2N3MnVyODFzYyJ9.4ahuQoPywGav2vRZzkGlIQ'
-      }).addTo(mymap);
-    });
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox.streets',
+      accessToken: 'pk.eyJ1Ijoib3JpY2Fsdm8iLCJhIjoiY2pjcW1uYjBhMTlzeDJxb2N3MnVyODFzYyJ9.4ahuQoPywGav2vRZzkGlIQ'
+    }).addTo(mymap);
   }
 }
