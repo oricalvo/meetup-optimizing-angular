83a506d8fd7c23c2b82a6427acf0598cb850568f
ocalvo <ori.calvo@gmail.com>
2018-01-23 16:01:14 +0200

    More

--- 02 disable-ngzone/src/app/app.module.ts
+++ 02 disable-ngzone/src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 import { ClockComponent } from './clock/clock.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClockComponent
+    ClockComponent,
   ],
   imports: [
     BrowserModule
